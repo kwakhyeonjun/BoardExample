99fd61944afebc53eca7896c4c25a196d91cb8e3
KwakHyeonjun <kwak0568@naver.com>
2023-07-27 01:56:09 +0900

    [BE] feat:: Board 비밀번호 로직 추가

--- BE/src/main/java/com/nts/board/exception/BoardException.java
+++ BE/src/main/java/com/nts/board/exception/BoardException.java
@@ -2,6 +2,7 @@ package com.nts.board.exception;
 
 public class BoardException extends RuntimeException {
     public static final String BOARD_NOT_FOUND = "존재하지 않는 게시물입니다.";
+    public static final String BOARD_FORBIDDEN = "허용되지 않은 접근입니다.";
 
     public BoardException(String message) {
         super(message);
--- BE/src/main/java/com/nts/board/service/BoardService.java
+++ BE/src/main/java/com/nts/board/service/BoardService.java
@@ -9,7 +9,7 @@ public interface BoardService {
     BoardResponse findBoard(long boardId);
     BoardResponse saveBoard(BoardRequest request);
     BoardResponse updateBoard(long boardId, BoardRequest request);
-    BoardResponse deleteBoard(long boardId);
+    BoardResponse deleteBoard(long boardId, BoardRequest request);
     List<BoardResponse> findBoardList();
     List<BoardResponse> searchBoardList(String title, String content, String writer, String hashtag);
 }
