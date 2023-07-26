9d3ea58518f34b5e160d2946a13512d54b191a20
KwakHyeonjun <kwak0568@naver.com>
2023-07-26 10:00:27 +0900

    [BE] chore: request 통합으로 이름 변경

--- BE/src/main/java/com/nts/board/request/BoardRequest.java
+++ BE/src/main/java/com/nts/board/request/BoardRequest.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class BoardSaveRequest {
+public class BoardRequest {
     String title;
     String writer;
     String content;
